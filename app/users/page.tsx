023326467474c48f5097bdc39cd52df801ac2895
Tales <jobbtales@gmail.com>
2023-06-14 18:35:57 -0300

    added UI chatbox

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,9 @@
-'use client';
-
-import { signOut } from "next-auth/react";
+import EmptyState from "../components/EmptyState";
 
 const Users = () => {
   return (
-    <div>
-      
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
     </div>
   )
 }
